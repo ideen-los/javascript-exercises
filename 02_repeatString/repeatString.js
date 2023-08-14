0a7d958f2c487bb525fec65deb9ae6a5db7e07ac
Robert <90513724+ideen-los@users.noreply.github.com>
2023-08-15 00:16:54 +0200

    ENH: solve tasks 2-6

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let returnValue = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = num; i > 0; i--) {
+    returnValue += string;
+  }
+  return returnValue;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const splittedString = string.split("");
+  let joinedString = "";
+  for (i = splittedString.length - 1; i >= 0; i--) {
+    joinedString += splittedString[i];
+  }
+  return joinedString;
 };
 
 // Do not edit below this line
