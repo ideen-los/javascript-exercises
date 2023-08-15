90f9836aef9dac18120605741f3efddfb0f8a9c1
Robert <90513724+ideen-los@users.noreply.github.com>
2023-08-16 00:24:20 +0200

    ENH: solve tasks 7 + 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let calcFahrenheit = (fahrenheit - 32) * (5 / 9);
+  if (Number.isInteger(calcFahrenheit) === true) {
+    return calcFahrenheit;
+  } else {
+    return +calcFahrenheit.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let calcCelsius = celsius * (9 / 5) + 32;
+  if (Number.isInteger(calcCelsius) === true) {
+    return calcCelsius;
+  } else {
+    return +calcCelsius.toFixed(1);
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let result = 0;
+  for (const num of nums) {
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  let result = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    result *= nums[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
